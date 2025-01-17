8de613ab7f1e3cda9c84dd31da1d140910b04cc6
Dynamiteboem2 <danielvangrol2006@gmail.com>
2025-01-17 21:08:44 +0100

    wordt nu naar de goede view gestuurd

--- app/views/homepages/index.php
+++ app/views/homepages/index.php
@@ -10,7 +10,7 @@
             <!-- Links naar verschillende pagina's -->
             <a href="<?= URLROOT; ?>/magazijn/index">Overzicht magazijn Jamin</a> |
             <a href="<?= URLROOT; ?>/leverancier/index">Overzicht leveranciers</a> |
-            <a href="<?= URLROOT; ?>/wijzigLeverancier/index">Wijzigen Leverancier</a>
+            <a href="<?= URLROOT; ?>/leverancier/wijzigenLeverancier">Wijzigen Leverancier</a>
 
         </div>
         <div class="col-2"></div>
