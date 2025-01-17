e87689c0508f48df786f184175ae8de3301b0ecf
Dynamiteboem2 <danielvangrol2006@gmail.com>
2025-01-17 16:32:47 +0100

    Vergeten branch te switchen

--- app/controllers/Leverancier.php
+++ app/controllers/Leverancier.php
@@ -133,17 +133,95 @@ class Leverancier extends BaseController
         }
     }
 
-    private function setFlash($name, $message)
-    {
-        if (!isset($_SESSION)) {
-            session_start();
-        }
-        $_SESSION[$name] = $message;
-    }
+    public function wijzigLeverancier($id)
+{
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Sanitize POST data
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-    private function redirect($url)
-    {
-        header('Location: ' . URLROOT . '/' . $url);
-        exit();
+        $data = [
+            'id' => $id,
+            'naam' => trim($_POST['naam']),
+            'contactpersoon' => trim($_POST['contactpersoon']),
+            'leveranciernummer' => trim($_POST['leveranciernummer']),
+            'mobiel' => trim($_POST['mobiel']),
+            'straatnaam' => trim($_POST['straatnaam']),
+            'huisnummer' => trim($_POST['huisnummer']),
+            'postcode' => trim($_POST['postcode']),
+            'stad' => trim($_POST['stad']),
+            'naam_err' => '',
+            'contactpersoon_err' => '',
+            'leveranciernummer_err' => '',
+            'mobiel_err' => '',
+            'straatnaam_err' => '',
+            'huisnummer_err' => '',
+            'postcode_err' => '',
+            'stad_err' => ''
+        ];
+
+        // Validate data
+        if (empty($data['naam'])) {
+            $data['naam_err'] = 'Vul de naam in';
+        }
+        if (empty($data['contactpersoon'])) {
+            $data['contactpersoon_err'] = 'Vul de contactpersoon in';
+        }
+        if (empty($data['leveranciernummer'])) {
+            $data['leveranciernummer_err'] = 'Vul het leveranciernummer in';
+        }
+        if (empty($data['mobiel'])) {
+            $data['mobiel_err'] = 'Vul het mobiele nummer in';
+        }
+        if (empty($data['straatnaam'])) {
+            $data['straatnaam_err'] = 'Vul de straatnaam in';
+        }
+        if (empty($data['huisnummer'])) {
+            $data['huisnummer_err'] = 'Vul het huisnummer in';
+        }
+        if (empty($data['postcode'])) {
+            $data['postcode_err'] = 'Vul de postcode in';
+        }
+        if (empty($data['stad'])) {
+            $data['stad_err'] = 'Vul de stad in';
+        }
+
+        if (empty($data['naam_err']) && empty($data['contactpersoon_err']) && empty($data['leveranciernummer_err']) && empty($data['mobiel_err']) && empty($data['straatnaam_err']) && empty($data['huisnummer_err']) && empty($data['postcode_err']) && empty($data['stad_err'])) {
+            // Validated
+            if ($this->leverancierModel->updateLeverancier($data)) {
+                $this->setFlash('leverancier_message', 'Leverancier bijgewerkt');
+                $this->redirect('leverancier/wijzigenLeverancier');
+            } else {
+                die('Er is iets misgegaan');
+            }
+        } else {
+            // Load view with errors
+            $this->view('leverancier/wijzigLeverancier', $data);
+        }
+    } else {
+        // Get existing leverancier from model
+        $leverancier = $this->leverancierModel->getLeverancierById($id);
+
+        $data = [
+            'id' => $id,
+            'naam' => $leverancier->Naam,
+            'contactpersoon' => $leverancier->Contactpersoon,
+            'leveranciernummer' => $leverancier->Leveranciernummer,
+            'mobiel' => $leverancier->Mobiel,
+            'straatnaam' => $leverancier->Straatnaam,
+            'huisnummer' => $leverancier->Huisnummer,
+            'postcode' => $leverancier->Postcode,
+            'stad' => $leverancier->Stad,
+            'naam_err' => '',
+            'contactpersoon_err' => '',
+            'leveranciernummer_err' => '',
+            'mobiel_err' => '',
+            'straatnaam_err' => '',
+            'huisnummer_err' => '',
+            'postcode_err' => '',
+            'stad_err' => ''
+        ];
+
+        $this->view('leverancier/wijzigLeverancier', $data);
     }
+}
 }
